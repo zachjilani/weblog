76b16fc4415c822b1f475ff9d65e540d187b0560
unknown <zach.jilani1@gmail.com>
2023-07-05 16:20:41 -0600

    initial commit

---
+++ weblog/src/main/java/com/example/weblog/WeblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.weblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblogApplication.class, args);
+	}
+
+}
