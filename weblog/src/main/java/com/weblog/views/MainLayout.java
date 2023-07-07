acdbfa0ae52c7e8fdee07a67c7ffd6679de046d6
unknown <zach.jilani1@gmail.com>
2023-07-07 13:01:03 -0600

    added logout and SecurityService

--- weblog/src/main/java/com/weblog/views/MainLayout.java
+++ weblog/src/main/java/com/weblog/views/MainLayout.java
@@ -3,13 +3,17 @@ package com.weblog.views;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouterLink;
+import com.weblog.security.SecurityService;
 
 public class MainLayout extends AppLayout {
+  private final SecurityService securityService;
 
-  public MainLayout() {
+  public MainLayout(SecurityService sercurityService) {
+    this.securityService = sercurityService;
     createHeader();
     createDrawer();
   }
@@ -18,7 +22,10 @@ public class MainLayout extends AppLayout {
     H1 logo = new H1("Weblog");
     logo.addClassNames("text-l", "m-m");
 
-    HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
+    String u = securityService.getAuthenticatedUser().getUsername();
+    Button logout = new Button("Log out " + u, e -> securityService.logout());
+
+    HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logout);
     header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
     header.expand(logo);
     header.setWidthFull();
