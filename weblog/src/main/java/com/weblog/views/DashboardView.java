dcd45e59881b36a79220c44691134d262be3f737
unknown <zach.jilani1@gmail.com>
2023-08-17 09:52:41 -0600

    adding company grid to dashboard view

--- weblog/src/main/java/com/weblog/views/DashboardView.java
+++ weblog/src/main/java/com/weblog/views/DashboardView.java
@@ -1,9 +1,20 @@
 package com.weblog.views;
 
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.weblog.data.models.Company;
+import com.weblog.data.service.CompanyService;
 
 import jakarta.annotation.security.PermitAll;
 
@@ -11,9 +22,54 @@ import jakarta.annotation.security.PermitAll;
 @Route(value = "dashboard", layout = MainLayout.class)
 @PermitAll
 public class DashboardView extends VerticalLayout {
-    public DashboardView() {
-        H1 test = new H1("test");
-        add(test);
-    }
+
+  Grid<Company> companies = new Grid<>(Company.class);
+  TextField filterText = new TextField();
+
+  @Autowired
+  CompanyService service;
+
+  public DashboardView(CompanyService service) {
+    this.service = service;
+    addClassName("dashboard-view");
+    setSizeFull();
+    configureGrid();
+    add(getToolbar(), getContent());
+    updateList();
+    // H1 test = new H1("test");
+    // add(test);
+  }
+
+  private Component getContent() {
+    HorizontalLayout content = new HorizontalLayout(companies);
+    content.setFlexGrow(2, companies);
+    // content.setFlexGrow(1, form);
+    content.addClassNames("content");
+    content.setSizeFull();
+    return content;
+  }
+
+  private void configureGrid() {
+    companies.addClassNames("companies-grid");
+    companies.setSizeFull();
+    companies.getColumns().forEach(col -> col.setAutoWidth(true));
+  }
+
+  private HorizontalLayout getToolbar() {
+    filterText.setPlaceholder("Filter by name...");
+    filterText.setClearButtonVisible(true);
+    filterText.setValueChangeMode(ValueChangeMode.LAZY);
+    filterText.addValueChangeListener(e -> updateList());
+
+    Button addContactButton = new Button("Add company");
+
+    var toolbar = new HorizontalLayout(filterText, addContactButton);
+    toolbar.addClassName("toolbar");
+    return toolbar;
+  }
+
+  private void updateList() {
+    companies.setItems(service.findAllCompanies(filterText.getValue()));
+  }
 
 }
